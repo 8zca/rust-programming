24e2bc715463d9a52743a362b07d744a99f25004
8zca <excref@gmail.com>
2021-10-21 23:56:07 +0900

    modify comment

--- basic/string.rs
+++ basic/string.rs
@@ -5,18 +5,18 @@ fn main() {
     let mut a2 = "foo2";
     println!("{}.{}", a1, a2);
 
-    // `&str`は`to_string()`メソッドで`String`にできる。
+    // &str は to_string() で String にできる
     let mut a: String = "abc".to_string();
-    // 少しややこしいが、`String`に`&str`を足すと`String`ができる。
-    // `&str`に`String`を足したり`String`に`String`を足したりはできない。
+    // String に &str を足すと String ができる
+    // &str + String, String + String は不可
     a += "def";
     println!("{}", a); // => abcdef
 
-    // `.to_string()`は様々な型に用意されている。
+    // .to_string() はどの型でも可. objectということか
     let x = 1.0.to_string();
     println!("{}", x); // 1
 
-    // `String`を`&str`にするには`as_str()`が使える。
+    // String を &str にするには as_str()
     a += x.as_str();
     println!("{}", a); // => abcdef1
 }
